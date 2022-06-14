c4918599d8a65534589d1d152279c93fe5c5a2ed
urclys <aymane.refak@gmail.com>
2022-06-14 04:21:45 +0100

    committt

--- app/base/static/assets/js/csrf_setup.js
+++
@@ -1,6 +0,0 @@
-
-  var csrf_token = $('meta[name=csrf-token]').attr('content');
-  console.log(csrf_token);
-  $.ajaxPrefilter(function (options, originalOptions, jqXHR) {
-  jqXHR.setRequestHeader('X-CSRF-Token', csrf_token);
-  });
